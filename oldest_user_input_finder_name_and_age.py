2cfd9acebd48a0232eb8e534fbbf8817a70ec64e
OgmerKenshin <kenshinaogmer@gmail.com>
2024-10-21 22:53:03 +0800

    replaced oldest user with get oldest person and turned my list in identification into an array

--- oldest_user_input_finder_name_and_age.py
+++ oldest_user_input_finder_name_and_age.py
@@ -1,47 +1,47 @@
 
 # I'm gonne set rules for what names are valid and what names aren't
-def valid_name(name):
-    return name.replace("","").isalpha()
-def valid_age(age):
+def is_valid_name(name):
+    return name.replace(" ", "").isalpha()
+def is_valid_age(age):
     return age.isdigit() and 0 <= int(age) <= 122
 # I will return the age and name for the "oldest person"
-def oldest_person(data):
+def get_oldest_person(data):
     if len(data) == 0:
         return None, None
-    oldest_user = max(data, key=lambda person: person[1])
-    return oldest_user
+    oldest_person = max(data, key=lambda person: person[1])
+    return oldest_person
 
 
 # Now the array is where the names and ages are stored
 #this will be the array
-Identification = {}
+Identification = []
 
 #I can now proceed to making loops and user input texts
 while True:
     name = input("please enter your name here: ")
-    while not valid_name(name):
+    while not is_valid_name(name):
         print("HAHA wrong!!! names should consist of letters and spaces only, if not then that's just sad man")
         name = input("please enter your name here: ")
 
 
     age = input("please enter your age here: ")
-    while not valid_age(age):
+    while not is_valid_age(age):
         print("seriously? dude...")
         age = input("please input your age here: ")
 
-# the age variable needs to be an integer and both variables are now stored in the array set earlier
-age = int(age)
-Identification.append((name,age))
+    # the age variable needs to be an integer and both variables are now stored in the array set earlier
+    age = int(age)
+    Identification.append((name,age))
 
-#retry and finally the break
-retry = input("do you wanna input another entry? (Yes or No): ").strip().lower()
-if retry != "yes": 
-    break
+    #retry and finally the break
+    retry = input("do you wanna input another entry? (Yes or No): ").strip().lower()
+    if retry != "yes": 
+      break
 
 # find and reveal the oldest person
-
-if name:
-    print(f"the oldest person is {name} with {age}.")
+oldest = get_oldest_person(Identification)
+if oldest:
+    print(f"the oldest person is {age[0]} with {age[1]}.")
 else: 
     print("How can you not put any valid entries and reach this part?")
        
