c0cdda69ad6fb68ea159fcef088ee183942c9bb1
OgmerKenshin <kenshinaogmer@gmail.com>
2024-10-21 21:13:14 +0800

    I returned the age and name for the oldest person

--- oldest_user_input_finder_name_and_age.py
+++ oldest_user_input_finder_name_and_age.py
@@ -2,6 +2,14 @@
 # I'm gonne set rules for what names are valid and what names aren't
 def valid_name(name):
     return name.replace("","").isalpha()
+def valid_age(age):
+    return age.isdigit() and 0 <= int(age) <= 122
+# I will return the age and name for the "oldest person"
+def oldest_user(data):
+    if len(data) == 0:
+        return None, None
+    oldest_user = max(data, key=lambda person: person[1])
+    return oldest_user
 
 
 
