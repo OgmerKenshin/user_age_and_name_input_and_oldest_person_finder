39cd6421c9b8f56d8dd966e736f1fa0142ff5533
OgmerKenshin <kenshinaogmer@gmail.com>
2024-10-21 20:00:31 +0800

    I set an array to store and sort the user inputs and set rules for a valid name

---
+++ oldest_user_input_finder_name_and_age.py
@@ -0,0 +1,25 @@
+
+# I'm gonne set rules for what names are valid and what names aren't
+def valid_name(name):
+    return name.replace("","").isalpha()
+
+
+
+
+
+
+
+
+
+
+
+#this will be the array
+Identification = {}
+
+       
+            
+
+        
+            
+        
+          
